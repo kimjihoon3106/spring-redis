c6502ac6a79871e6163d4980a5c2a6c8e4838606
kimjihoon3106 <s24021@gsm.hs.kr>
2024-06-13 20:06:59 +0900

    Feat : "Person Entity"

--- src/main/java/com/example/springredis/entity/Person.java
+++ src/main/java/com/example/springredis/entity/Person.java
@@ -1,7 +1,7 @@
 package com.example.springredis.entity;
 
-import jakarta.persistence.Id;
 import lombok.Getter;
+import org.springframework.data.annotation.Id;
 import org.springframework.data.redis.core.RedisHash;
 
 import java.time.LocalDateTime;
